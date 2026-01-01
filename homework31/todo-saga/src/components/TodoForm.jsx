54c9267475bd95e0bfab9cbd2b137036bced40e3
VSsmbrsk <kolhoznikmarek@gmail.com>
2026-01-01 09:51:12 +0200

    refactor: simplify TodoForm component and remove unused actions

--- homework31/todo-saga/src/components/TodoForm.jsx
+++ homework31/todo-saga/src/components/TodoForm.jsx
@@ -1,54 +1,3 @@
-/*import { Formik, Form, Field } from "formik";
-import { useDispatch } from "react-redux";
-import { addTodo } from "../store/actions";
-
-function validateField(value) {
-  let error;
-  if (!value) {
-    error = "Required!";
-  } else if (value.length < 5) {
-    error = "Too short!";
-  }
-  return error;
-}
-
-export const TodoForm = () => {
-  const dispatch = useDispatch();
-
-  return (
-    <Formik
-      initialValues={{ value: "" }}
-      onSubmit={(values, { resetForm }) => {
-        const text = values.value.trim();
-        if (!text) return;
-
-        dispatch(addTodo(text));
-        resetForm();
-      }}
-    >
-        {({ errors, touched, isValidating }) => (
-      <Form className="form">
-        <div className="form__wrapper">
-                    <Field
-                    name="value"
-                    className="form__input"
-                    placeholder="New task"
-                    validate={validateField}
-                />
-                {errors.value && touched.value && <div>{errors.value}</div>}
-                </div>
-        <button type="submit" className="form__btn">
-          Add
-        </button>
-      </Form>
-        )}
-    </Formik>
-  );
-}
-
-export default TodoForm;*/
-
-
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { addTodo } from '../store/todosSlice'
@@ -69,7 +18,6 @@ function TodoForm() {
   const fetchHandler = e => {
     e.preventDefault()
     dispatch(fetchTodos())
-    // Dispatch an action to fetch todos
   }
 
   return (
